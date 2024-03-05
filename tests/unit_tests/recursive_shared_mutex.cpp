2500e780fbba1234f6414c3f8caf1dd49736a5db
jeffro256 <jeffro256@tutanota.com>
2024-03-05 16:18:56 -0600

    test recursive_shared_mutex against reader_writer_lock

--- tests/unit_tests/recursive_shared_mutex.cpp
+++ tests/unit_tests/recursive_shared_mutex.cpp
@@ -36,6 +36,8 @@
 
 #include "common/recursive_shared_mutex.h"
 
+#include "syncobj.h"
+
 TEST(recursive_shared_mutex, writer_starve_25_thread_90_duty)
 {
     tools::recursive_shared_mutex rw_mutex;
@@ -75,6 +77,64 @@ TEST(recursive_shared_mutex, writer_starve_25_thread_90_duty)
 
         // acquire exclusive ownership of the lock, set writer_ran, and wake main thread
         std::lock_guard<tools::recursive_shared_mutex> lg(rw_mutex);
+        writer_ran = true;
+        timeout_condition.notify_all();
+    }));
+
+    {
+        std::mutex timeout_mutex;
+        std::unique_lock<std::mutex> timeout_lock(timeout_mutex);
+        timeout_condition.wait_for(timeout_lock, std::chrono::duration<double>(45.0f));
+    }
+
+    const bool res = writer_ran.load();
+
+    run_workers = false;
+    for (auto &w : workers)
+        w.join();
+
+    EXPECT_TRUE(res);
+}
+
+TEST(recursive_shared_mutex, writer_starve_25_thread_90_duty_0xffff)
+{
+    epee::reader_writer_lock rw_mutex;
+
+    std::atomic<bool> run_workers{true};
+    std::atomic<bool> writer_ran{false};
+    std::condition_variable timeout_condition;
+
+    std::vector<std::thread> workers;
+
+    // 25 "reader" threads
+    for (int i = 0; i < 25; ++i)
+    {
+        const double initial_wait_seconds = static_cast<double>(i) / 25;
+        workers.push_back(std::thread([&rw_mutex, &run_workers, initial_wait_seconds](){
+            // sleep a staggered amount of time to try to reduce open gaps in holding pattern
+            std::this_thread::sleep_for(std::chrono::duration<double>(initial_wait_seconds));
+
+            while (run_workers)
+            {
+                {
+                    // shared lock held 90% of the time
+                    RLOCK(rw_mutex);
+                    std::this_thread::sleep_for(std::chrono::duration<double>(0.9));
+                }
+
+                // shared lock unheld 10% of the time
+                std::this_thread::sleep_for(std::chrono::duration<double>(0.1));
+            }
+        }));
+    }
+
+    // 1 "writer" thread
+    workers.push_back(std::thread([&rw_mutex, &writer_ran, &timeout_condition](){
+        // sleep for 2 seconds to give readers time to stagger
+        std::this_thread::sleep_for(std::chrono::duration<double>(2.0f));
+
+        // acquire exclusive ownership of the lock, set writer_ran, and wake main thread
+        RWLOCK(rw_mutex);
         writer_ran = true;
         timeout_condition.notify_all();
     }));
