c7422607385a070d9e0069f96598be78336f1ef8
Jeffro <jeffreyryan@tutanota.com>
2024-05-02 11:42:26 -0500

    Direct & compact tx serialization  (#39)
    * direct & compact tx serialization
    
    txs are [de]serialized directly from their classes and sizes of containers are not serialized
    if they can be implied.

--- tests/unit_tests/seraphis_serialization.cpp
+++ tests/unit_tests/seraphis_serialization.cpp
@@ -28,41 +28,33 @@
 
 #include "ringct/rctTypes.h"
 #include "seraphis_core/binned_reference_set.h"
-#include "seraphis_impl/serialization_demo_types.h"
-#include "seraphis_impl/serialization_demo_utils.h"
+#include "seraphis_impl/seraphis_serialization.h"
 #include "seraphis_main/txtype_base.h"
 #include "seraphis_main/txtype_coinbase_v1.h"
 #include "seraphis_main/txtype_squashed_v1.h"
 #include "seraphis_mocks/seraphis_mocks.h"
+#include "serialization/binary_utils.h"
 #include "span.h"
 
 #include "gtest/gtest.h"
 
 using namespace sp;
-using namespace mocks;
+using namespace jamtis;
+using namespace sp::mocks;
 
 //-------------------------------------------------------------------------------------------------------------------
-TEST(seraphis_serialization_demo, seraphis_coinbase_empty)
+TEST(seraphis_serialization, seraphis_coinbase_empty)
 {
     // make empty tx
     SpTxCoinbaseV1 tx{};
 
-    // convert the tx to serializable form
-    sp::serialization::ser_SpTxCoinbaseV1 serializable_tx;
-    EXPECT_NO_THROW(sp::serialization::make_serializable_sp_tx_coinbase_v1(tx, serializable_tx));
-
     // serialize the tx
     std::string serialized_tx;
-    EXPECT_TRUE(sp::serialization::try_append_serializable(serializable_tx, serialized_tx));
-
-    // deserialize the tx
-    sp::serialization::ser_SpTxCoinbaseV1 serializable_tx_recovered;
-    EXPECT_TRUE(sp::serialization::try_get_serializable(epee::strspan<std::uint8_t>(serialized_tx),
-        serializable_tx_recovered));
+    EXPECT_TRUE(::serialization::dump_binary(tx, serialized_tx));
 
     // recover the tx
     SpTxCoinbaseV1 recovered_tx;
-    EXPECT_NO_THROW(sp::serialization::recover_sp_tx_coinbase_v1(serializable_tx_recovered, recovered_tx));
+    EXPECT_TRUE(::serialization::parse_binary(serialized_tx, recovered_tx));
 
     // check that the original tx was recovered
     rct::key original_tx_id;
@@ -75,27 +67,18 @@ TEST(seraphis_serialization_demo, seraphis_coinbase_empty)
     EXPECT_NO_THROW(EXPECT_TRUE(sp_tx_coinbase_v1_size_bytes(tx) == sp_tx_coinbase_v1_size_bytes(recovered_tx)));
 }
 //-------------------------------------------------------------------------------------------------------------------
-TEST(seraphis_serialization_demo, seraphis_squashed_empty)
+TEST(seraphis_serialization, seraphis_squashed_empty)
 {
     // make empty tx
     SpTxSquashedV1 tx{};
 
-    // convert the tx to serializable form
-    sp::serialization::ser_SpTxSquashedV1 serializable_tx;
-    EXPECT_NO_THROW(sp::serialization::make_serializable_sp_tx_squashed_v1(tx, serializable_tx));
-
     // serialize the tx
     std::string serialized_tx;
-    EXPECT_TRUE(sp::serialization::try_append_serializable(serializable_tx, serialized_tx));
-
-    // deserialize the tx
-    sp::serialization::ser_SpTxSquashedV1 serializable_tx_recovered;
-    EXPECT_TRUE(sp::serialization::try_get_serializable(epee::strspan<std::uint8_t>(serialized_tx),
-        serializable_tx_recovered));
+    EXPECT_TRUE(::serialization::dump_binary(tx, serialized_tx));
 
     // recover the tx
     SpTxSquashedV1 recovered_tx;
-    EXPECT_NO_THROW(sp::serialization::recover_sp_tx_squashed_v1(serializable_tx_recovered, recovered_tx));
+    EXPECT_TRUE(::serialization::parse_binary(serialized_tx, recovered_tx));
 
     // check that the original tx was recovered
     rct::key original_tx_id;
@@ -108,7 +91,7 @@ TEST(seraphis_serialization_demo, seraphis_squashed_empty)
     EXPECT_NO_THROW(EXPECT_TRUE(sp_tx_squashed_v1_size_bytes(tx) == sp_tx_squashed_v1_size_bytes(recovered_tx)));
 }
 //-------------------------------------------------------------------------------------------------------------------
-TEST(seraphis_serialization_demo, seraphis_coinbase_standard)
+TEST(seraphis_serialization, seraphis_coinbase_standard)
 {
     // ledger context
     MockLedgerContext ledger_context{0, 10000};
@@ -118,22 +101,13 @@ TEST(seraphis_serialization_demo, seraphis_coinbase_standard)
     SpTxCoinbaseV1 tx;
     make_mock_tx<SpTxCoinbaseV1>(SpTxParamPackV1{.output_amounts = {1}}, ledger_context, tx);
 
-    // convert the tx to serializable form
-    sp::serialization::ser_SpTxCoinbaseV1 serializable_tx;
-    EXPECT_NO_THROW(sp::serialization::make_serializable_sp_tx_coinbase_v1(tx, serializable_tx));
-
     // serialize the tx
     std::string serialized_tx;
-    EXPECT_TRUE(sp::serialization::try_append_serializable(serializable_tx, serialized_tx));
-
-    // deserialize the tx
-    sp::serialization::ser_SpTxCoinbaseV1 serializable_tx_recovered;
-    EXPECT_TRUE(sp::serialization::try_get_serializable(epee::strspan<std::uint8_t>(serialized_tx),
-        serializable_tx_recovered));
+    EXPECT_TRUE(::serialization::dump_binary(tx, serialized_tx));
 
     // recover the tx
     SpTxCoinbaseV1 recovered_tx;
-    EXPECT_NO_THROW(sp::serialization::recover_sp_tx_coinbase_v1(serializable_tx_recovered, recovered_tx));
+    EXPECT_TRUE(::serialization::parse_binary(serialized_tx, recovered_tx));
 
     // check the tx was recovered
     rct::key original_tx_id;
@@ -148,7 +122,7 @@ TEST(seraphis_serialization_demo, seraphis_coinbase_standard)
     EXPECT_TRUE(validate_tx(recovered_tx, tx_validation_context));
 }
 //-------------------------------------------------------------------------------------------------------------------
-TEST(seraphis_serialization_demo, seraphis_squashed_standard)
+TEST(seraphis_serialization, seraphis_squashed_standard)
 {
     // config
     SpTxParamPackV1 tx_params;
@@ -165,7 +139,7 @@ TEST(seraphis_serialization_demo, seraphis_squashed_standard)
     tx_params.sp_input_amounts = {2, 3};
     tx_params.output_amounts = {3};
     tx_params.discretized_fee = discretize_fee(3);
-    
+
     const SemanticConfigSpRefSetV1 sp_ref_set_config{
             .decomp_n = tx_params.ref_set_decomp_n,
             .decomp_m = tx_params.ref_set_decomp_m,
@@ -179,27 +153,15 @@ TEST(seraphis_serialization_demo, seraphis_squashed_standard)
 
     // make a tx
     SpTxSquashedV1 tx;
-    make_mock_tx<SpTxSquashedV1>(tx_params,
-        ledger_context,
-        tx);
-
-    // convert the tx to serializable form
-    sp::serialization::ser_SpTxSquashedV1 serializable_tx;
-    EXPECT_NO_THROW(sp::serialization::make_serializable_sp_tx_squashed_v1(tx, serializable_tx));
+    make_mock_tx<SpTxSquashedV1>(tx_params, ledger_context, tx);
 
     // serialize the tx
     std::string serialized_tx;
-    EXPECT_TRUE(sp::serialization::try_append_serializable(serializable_tx, serialized_tx));
-
-    // deserialize the tx
-    sp::serialization::ser_SpTxSquashedV1 serializable_tx_recovered;
-    EXPECT_TRUE(sp::serialization::try_get_serializable(epee::strspan<std::uint8_t>(serialized_tx),
-        serializable_tx_recovered));
+    EXPECT_TRUE(::serialization::dump_binary(tx, serialized_tx));
 
     // recover the tx
     SpTxSquashedV1 recovered_tx;
-    EXPECT_NO_THROW(sp::serialization::recover_sp_tx_squashed_v1(serializable_tx_recovered,
-        recovered_tx));
+    EXPECT_TRUE(::serialization::parse_binary(serialized_tx, recovered_tx));
 
     // check the tx was recovered
     rct::key original_tx_id;
@@ -212,5 +174,58 @@ TEST(seraphis_serialization_demo, seraphis_squashed_standard)
     EXPECT_NO_THROW(EXPECT_TRUE(sp_tx_squashed_v1_size_bytes(tx) == sp_tx_squashed_v1_size_bytes(recovered_tx)));
     EXPECT_TRUE(validate_tx(tx, tx_validation_context));
     EXPECT_TRUE(validate_tx(recovered_tx, tx_validation_context));
+}
+//-------------------------------------------------------------------------------------------------------------------
+TEST(seraphis_serialization, jamtis_destination_v1)
+{
+    // generate
+    JamtisDestinationV1 dest{gen_jamtis_destination_v1()};
+
+    // serialize
+    std::string serialized_dest;
+    EXPECT_TRUE(::serialization::dump_binary(dest, serialized_dest));
+
+    // deserialize
+    JamtisDestinationV1 recovered_dest;
+    EXPECT_TRUE(::serialization::parse_binary(serialized_dest, recovered_dest));
+
+    // compare
+    EXPECT_EQ(dest, recovered_dest);
+}
+//-------------------------------------------------------------------------------------------------------------------
+TEST(seraphis_serialization, jamtis_payment_proposal_v1)
+{
+    // generate
+    JamtisPaymentProposalV1 payprop{gen_jamtis_payment_proposal_v1(7, 3)};
+
+    // serialize
+    std::string serialized_payprop;
+    EXPECT_TRUE(::serialization::dump_binary(payprop, serialized_payprop));
+
+    // deserialize
+    JamtisPaymentProposalV1 recovered_payprop;
+    EXPECT_TRUE(::serialization::parse_binary(serialized_payprop, recovered_payprop));
+
+    // compare
+    EXPECT_EQ(payprop, recovered_payprop);
+}
+//-------------------------------------------------------------------------------------------------------------------
+TEST(seraphis_serialization, jamtis_payment_proposal_self_send_v1)
+{
+    // generate
+    JamtisPaymentProposalSelfSendV1 payprop{
+            gen_jamtis_selfsend_payment_proposal_v1(7, JamtisSelfSendType::SELF_SPEND, 3)
+        };
+
+    // serialize
+    std::string serialized_payprop;
+    EXPECT_TRUE(::serialization::dump_binary(payprop, serialized_payprop));
+
+    // deserialize
+    JamtisPaymentProposalSelfSendV1 recovered_payprop;
+    EXPECT_TRUE(::serialization::parse_binary(serialized_payprop, recovered_payprop));
+
+    // compare
+    EXPECT_EQ(payprop, recovered_payprop);
 }
 //-------------------------------------------------------------------------------------------------------------------
