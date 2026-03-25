e7a22b087be6e9eef26cfba2f277635ac4902682
Jeet <jeetrajsinh2005@gmail.com>
2026-03-25 23:14:52 +0530

    added comment src/simplewallet/simplewallet.cpp
    Added Comment
    
    Co-authored-by: Jeffro <jeffro256@tutanota.com>

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -6340,6 +6340,7 @@ bool simple_wallet::prompt_if_old(const std::vector<tools::wallet2::pending_tx> 
   int max_n_old = 0;
   for (const auto &ptx: ptx_vector)
   {
+    // Old co-spend heuristic doesn't apply after FCMP++
     if (ptx.tx.rct_signatures.type >= rct::RCTTypeFcmpPlusPlus)
       continue;
 
