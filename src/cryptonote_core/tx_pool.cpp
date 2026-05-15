d1a9225bfbae0822d2a9a1e6af1da95bd9cc73b6
j-berman <justinberman@protonmail.com>
2026-05-14 20:25:05 -0700

    Keep meta.max_used_block_height set for FCMP++ txs
    It can't be changed after a reorg, it's inextricably tied to the
    reference block. So we keep it set.

--- src/cryptonote_core/tx_pool.cpp
+++ src/cryptonote_core/tx_pool.cpp
@@ -276,7 +276,7 @@ namespace cryptonote
         meta.weight = tx_weight;
         meta.fee = fee;
         meta.max_used_block_id = null_hash;
-        meta.max_used_block_height = 0;
+        meta.max_used_block_height = rct::is_rct_fcmp(tx.rct_signatures.type) ? max_used_block_height : 0;
         meta.last_failed_height = 0;
         meta.last_failed_id = null_hash;
         meta.receive_time = receive_time;
